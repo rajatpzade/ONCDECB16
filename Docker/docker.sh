39c0a900cb3cae9971ca8f4e2be57dfb2a481fec
rajat <zrajat@gmail.com>
2024-12-12 20:19:58 +0530

    --- Auto Git Commit ---

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -21,7 +21,7 @@ sudo apt-get update -y
 
 # Install the Docker packages.
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 
 systemctl start docker
 
