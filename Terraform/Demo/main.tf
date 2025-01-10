6149fa06dcba19c667d7edb3dde0251124498f01
rajat <zrajat@gmail.com>
2025-01-10 16:37:27 +0530

    --- Auto Git Commit ---

--- Terraform/Demo/main.tf
+++ Terraform/Demo/main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+
+
+
+
 resource "aws_instance" "web" {
   ami           = "ami-0b2cd2a95639e0e5b"
   instance_type = "t3.micro"
