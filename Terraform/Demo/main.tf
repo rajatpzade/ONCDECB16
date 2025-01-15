68993576d95661ade4f05717690524889ad30740
rajat <zrajat@gmail.com>
2025-01-15 20:19:16 +0530

    --- Auto Git Commit ---

--- Terraform/Demo/main.tf
+++ Terraform/Demo/main.tf
@@ -4,11 +4,3 @@ provider "aws" {
   secret_key = "my-secret-key"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0b2cd2a95639e0e5b"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Terraform"
-  }
-}
