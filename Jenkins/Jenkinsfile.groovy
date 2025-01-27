fce2fd7c698f4b393633a1d4d7987de76461fb98
Rajat Zade <rajat@Rajats-MacBook-Air.local>
2025-01-27 20:34:56 +0530

    --- Auto Git Commit ---

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -18,7 +18,7 @@ pipeline {
         }
         stage('Deploy') { 
             steps {
-                // 
+                sh 'echo Deploy stage successfully run' 
             }
         }
     }
