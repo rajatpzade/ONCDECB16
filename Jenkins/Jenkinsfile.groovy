5b619c7b67e0940b7febca8f2857a7d64a6c94b9
Rajat Zade <rajat@Rajats-MacBook-Air.local>
2025-01-29 17:18:06 +0530

    --- Auto Git Commit ---

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -26,30 +26,3 @@ pipeline {
 
 
 
-pipeline {
-    agent any 
-    stages {
-        stage('Pull') { 
-            steps {
-                git 'https://github.com/rajatpzade/studentapp.ui.git' 
-            }
-        }
-        stage('Build') { 
-            steps {
-               sh '/opt/maven/bin/mvn clean package'
-            }
-        }
-        stage('test') { 
-            steps {
-                 withSonarQubeEnv ('sonar') {
-             sh ' /opt/maven/bin/mvn sonar:sonar '
-                }
-            }
-        }
-        stage('Deploy') { 
-            steps {
-               deploy adapters: [tomcat8(credentialsId: 'tomcat-cred', path: '', url: 'http://13.38.102.52:8080')], contextPath: '/', onFailure: false, war: '**/*.war'
-            }
-        }        
-    }
-}
