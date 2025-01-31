ed91aab445a1525a721980d83efc30a7f9247988
rajatz <rajatz@gmail.com>
2025-01-31 20:45:48 +0530

    --- Auto Git Commit ---

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -1,27 +1,33 @@
 pipeline {
     agent any 
+
     stages {
         stage('Pull') { 
             steps {
-                git 'https://github.com/rajatpzade/studentapp.ui.git'
+                git url: 'https://github.com/rajatpzade/studentapp.ui.git'
             }
         }
+
         stage('Build') { 
             steps {
-               sh '/opt/maven/bin/mvn clean package'
+                sh '/opt/maven/bin/mvn clean package'
             }
         }        
+
         stage('Test') { 
             steps {
-                    withSonarQubeEnv(credentialsId: 'sonarqube') {
-                sh 'mvn clean verify sonar:sonar'
+                script {
+                    withSonarQubeEnv(credentialsId: 'sonar') {
+                        sh '/opt/maven/bin/mvn clean verify sonar:sonar'
+                    }
+                }
             }
         }
+
         stage('Deploy') { 
             steps {
                 sh 'echo Deploy stage successfully run' 
             }
         }
-      }
-   }
+    }
 }
