46b00e4e32826a6b1e317acf868f5c68a1834ac4
rajatz <rajatz@gmail.com>
2025-01-31 20:35:26 +0530

    --- Auto Git Commit ---

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -13,7 +13,7 @@ pipeline {
         }        
         stage('Test') { 
                 withSonarQubeEnv(credentialsId: 'sonarqube') {
-                     // some block
+                     sh 'mvn clean verify sonar:sonar'
         }
         stage('Deploy') { 
             steps {
