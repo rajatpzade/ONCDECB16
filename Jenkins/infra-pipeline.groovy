63e6850eb0774918fdca8c29e0a08aa997b4fdd1
rajatz <rajatz@gmail.com>
2025-02-04 20:34:25 +0530

    --- Auto Git Commit ---

--- Jenkins/infra-pipeline.groovy
+++ Jenkins/infra-pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {
                 git branch: 'main', url: 'https://github.com/rajatpzade/ONCDECB16.git'
             }
-        }
+        }   
         stage('Test') {
             steps {
                 sh '''  cd Terraform/ 
